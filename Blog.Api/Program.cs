be940e224fc852f3aaac1c328217fa9364143e84
Zhexuan <rmt_zhou@live.com>
2019-01-08 09:36:35 +0800

    init_project

---
+++ Blog.Api/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Blog.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine("log", @"log.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
---
+++ Blog.Infrastracture/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Blog.Infrastracture
+{
+    public class Class1
+    {
+    }
+}
